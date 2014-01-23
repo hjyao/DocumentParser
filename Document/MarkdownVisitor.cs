fce835d12e5d59863df8b17ab492b916f9dfc070
cwp_pink_star <cwp_pink_star@thoughtworks.com>
2014-01-23 18:58:50 +0800

    new requirement coming: has implimented markdown boldtext.

---
+++ Document/MarkdownVisitor.cs
@@ -0,0 +1,20 @@
+﻿namespace Document
+{
+    public class MarkdownVisitor : IVisitor
+    {
+        public string VisitPlainText(PlainText plainText)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string VisitBoldText(BoldText boldText)
+        {
+            return string.Format("_{0}_", boldText.Text);
+        }
+
+        public string VisitHyperLink(HyperLink hyperLink)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- DocumentTest/DocumentTest.cs
+++ DocumentTest/DocumentTest.cs
@@ -37,5 +37,16 @@ namespace DocumentTest
 
             Assert.Equal("<a href='http://www.visitor.com'>I am hyperlink</a>", html);
         }
+
+        [Fact]
+        public void should_output_markdown_for_document_with_bold_text_part()
+        {
+            var boldText = new BoldText("I am bold text");
+            var document = new Document.Document(boldText);
+
+            string html = document.Accept(new MarkdownVisitor());
+
+            Assert.Equal("_I am bold text_", html);
+        }
     }
 }
